24662c58c13e623b6f85c3d39e6f010149474c34
Аслан Нурсеитов <grooves97@gmail.com>
2019-05-03 11:50:00 +0600

    Созданный tab не работает кнопка добавить отказывается работать

---
+++ WpfTAB/WpfTAB/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTAB
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTab_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TabControl newTabControl = new TabControl();
+            MainTabControl.Items.Add(newTabControl);
+            
+        }
+    }
+}
